15630d0ced58e4609afb6e41d6b12fa8040a3673
Nvdepart <67013480+Nvdepart@users.noreply.github.com>
2023-04-24 23:01:10 +0200

    NavBar animee

--- codi/src/components/NavBar.vue
+++ codi/src/components/NavBar.vue
@@ -3,14 +3,56 @@
     <v-btn>
       <v-icon color="yellow" left class="mr-2">fas fa-signature</v-icon>
       Atlas-Expo
-      <v-spacer></v-spacer>
-      <v-btn text @clcik="scroll('home')" class="text-yellow">Home</v-btn>
-      <v-btn text @clcik="scroll('about')">About</v-btn>
-      <v-btn text @clcik="scroll('portfolio')">Portfolio</v-btn>
-      <v-btn text @clcik="scroll('services')">Services</v-btn>
-      <v-btn text @clcik="scroll('page')">Page</v-btn>
-      <v-btn text @clcik="scroll('blog')">Bio</v-btn>
-      <v-btn text @clcik="scroll('contact')">Contact</v-btn>
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn text @click="scroll('home')" class="text-yellow btn">
+      <span>Home</span>
+      <div class="top"></div>
+      <div class="left"></div>
+      <div class="bottom"></div>
+      <div class="right"></div
+    ></v-btn>
+    <v-btn text @click="scroll('about')" class="btn">
+      <span>About</span>
+      <div class="top"></div>
+      <div class="left"></div>
+      <div class="bottom"></div>
+      <div class="right"></div>
+    </v-btn>
+    <v-btn text @click="scroll('portfolio')" class="btn">
+      <span>Portfolio</span>
+      <div class="top"></div>
+      <div class="left"></div>
+      <div class="bottom"></div>
+      <div class="right"></div>
+    </v-btn>
+    <v-btn text @click="scroll('services')" class="btn">
+      <span>Services</span>
+      <div class="top"></div>
+      <div class="left"></div>
+      <div class="bottom"></div>
+      <div class="right"></div>
+    </v-btn>
+    <v-btn text @click="scroll('page')" class="btn">
+      <span>Page</span>
+      <div class="top"></div>
+      <div class="left"></div>
+      <div class="bottom"></div>
+      <div class="right"></div>
+    </v-btn>
+    <v-btn text @click="scroll('blog')" class="btn">
+      <span>Bio</span>
+      <div class="top"></div>
+      <div class="left"></div>
+      <div class="bottom"></div>
+      <div class="right"></div>
+    </v-btn>
+    <v-btn text @click="scroll('contact')" class="btn">
+      <span>Contact Us</span>
+      <div class="top"></div>
+      <div class="left"></div>
+      <div class="bottom"></div>
+      <div class="right"></div>
     </v-btn>
   </v-app-bar>
 </template>
@@ -26,4 +68,102 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.btn {
+  padding: 10px;
+  background-color: #124578;
+  border: none;
+  font-size: 12px;
+  position: relative;
+  /*overflow: hidden;*/
+  transition: 500ms;
+}
+
+.btn span {
+  color: rgb(8, 3, 33);
+  position: relative;
+  transition: 500ms;
+  transition-delay: 500ms;
+  font-size: 16px;
+  text-transform: uppercase;
+
+  font-weight: 600;
+}
+
+.btn:before {
+  content: "";
+  position: absolute;
+  width: 0%;
+  height: 0%;
+  left: 50%;
+  right: 50%;
+  top: 50%;
+  bottom: 50%;
+  transition: 500ms;
+  transition-delay: 500ms;
+  background-color: rgb(220, 236, 5);
+  box-shadow: 0 0 10px rgb(0, 4, 255), 0 0 30px rgb(13, 5, 245), 0 0 50px red;
+}
+
+.btn div {
+  transition: 500ms;
+  position: absolute;
+  background-color: rgb(4, 93, 236);
+  box-shadow: 0 0 15px rgb(5, 5, 248), 0 0 30px rgb(56, 6, 222), 0 0 50px red;
+}
+
+.btn .top {
+  width: 2px;
+  height: 2px;
+  top: 0;
+  left: 0;
+}
+
+.btn .bottom {
+  width: 2px;
+  height: 2px;
+  bottom: 0;
+  right: 0;
+}
+
+.btn .left {
+  width: 2px;
+  height: 2px;
+  top: 0;
+  left: 0;
+}
+
+.btn .right {
+  width: 2px;
+  height: 2px;
+  bottom: 0;
+  right: 0;
+}
+
+.btn:hover {
+  color: #000;
+}
+
+.btn:hover span {
+  color: #000;
+}
+
+.btn:hover:before {
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+
+.btn:hover .top,
+.btn:hover .bottom {
+  width: 100%;
+}
+
+.btn:hover .left,
+.btn:hover .right {
+  height: 100%;
+}
+</style>
--- codi/src/views/HomeView.vue
+++ codi/src/views/HomeView.vue
@@ -9,8 +9,16 @@
               <h1 class="text-grey">Hello,</h1>
               <h1 class="text-white">I'm Hicham OUKASSOU</h1>
               <span class="text-grey">Front-End Web Developer</span><br />
-              <v-btn tile dark class="text-yellow mt-8" variant="outlined"
-                >Contact me</v-btn
+              <v-btn
+                tile
+                dark
+                class="text-yellow mt-8 hoverMe"
+                variant="outlined"
+              >
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>Contact me</v-btn
               >
             </div>
           </v-col>
@@ -26,23 +34,30 @@
                 right: 0;
                 text-align: center;
               "
+              class="mt-16 container_mouse"
             >
-              <v-icon>fas fa-angle-double-down</v-icon>
+              <span class="mouse-btn">
+                <span class="mouse-scroll">
+                  <v-icon style="font-size: 30px"
+                    >fas fa-angle-double-down</v-icon
+                  >
+                </span>
+              </span>
             </div>
           </v-col>
           <v-col cols="5">
             <div style="position: relative; z-index: 9999" class="mt-16">
-              <v-img src="1.png" contain max-height="300"></v-img>
+              <v-img src="2.png" contain max-height="300"></v-img>
             </div>
           </v-col>
         </v-row>
       </div>
-      <v-col cols="12" class="mt-10" id="about">
+      <v-col cols="12" class="mt-16" id="about">
         <div>
           <v-row>
             <v-col cols="12" sm="6">
               <div class="egg">
-                <v-img src="1.png" max-height="300"></v-img>
+                <v-img src="2.png" max-height="300" class=""></v-img>
               </div>
             </v-col>
             <v-col cols="12" sm="6">
@@ -52,7 +67,7 @@
                 </v-slider>
               </div>
               <h4 class="mt-n4">I Am a Creative web Design</h4>
-              <h4>And Developer</h4>
+              <h4 class="">And Developer</h4>
               <p class="text-grey">
                 I am an enthusiast and passionate about information technology
                 with a focus on web development. Throughout my training, I have
@@ -90,7 +105,7 @@
               <div class="child">
                 <v-btn
                   icon="fas fa-laptop"
-                  color="#C7DF78"
+                  color="blue"
                   class="text-white"
                 ></v-btn>
                 <h3 class="ml-3 mt-4">Web Design</h3>
@@ -100,11 +115,206 @@
                   with a focus on web development
                 </p>
               </div>
+              <div class="child">
+                <v-btn
+                  icon="fas fa-mobile-alt"
+                  color="blue"
+                  class="text-white"
+                ></v-btn>
+                <h3 class="ml-3 mt-4">App Design</h3>
+                <p class="text-grey ml-3 mt-4 text-caption">
+                  I am an enthusiast and passionate <br />
+                  about information technology <br />
+                  with a focus on web development
+                </p>
+              </div>
+              <div class="child">
+                <v-btn
+                  icon="fas fa-camera"
+                  color="blue"
+                  class="text-white"
+                ></v-btn>
+                <h3 class="ml-3 mt-4">Front-End</h3>
+                <p class="text-grey ml-3 mt-4 text-caption">
+                  I am an enthusiast and passionate <br />
+                  about information technology <br />
+                  with a focus on web development
+                </p>
+              </div>
+            </v-col>
+          </v-row>
+          <v-divider></v-divider>
+        </div>
+      </v-col>
+      <v-col cols="12" sm="12" id="services">
+        <div class="d-flex justify-center mb-6">
+          <v-btn color="primary" class="mr-2">All</v-btn>
+          <v-btn class="mr-2" variant="tonal">Web Design</v-btn>
+          <v-btn class="mr-2" variant="tonal">Front Design</v-btn>
+          <v-btn class="mr-2" variant="tonal">Photography</v-btn>
+          <v-btn variant="tonal">Illustration</v-btn>
+        </div>
+      </v-col>
+      <v-col cols="12" class="imgHover">
+        <v-row class="fill-height" align="center" justify="center">
+          <template v-for="(item, i) in items" :key="i">
+            <v-col cols="12" md="4">
+              <v-hover v-slot="{ isHovering, props }">
+                <v-card
+                  :elevation="isHovering ? 12 : 2"
+                  :class="{ 'on-hover': isHovering }"
+                  v-bind="props"
+                >
+                  <v-img :src="item.img" height="225px" cover> </v-img>
+                </v-card>
+              </v-hover>
+            </v-col>
+          </template>
+        </v-row>
+      </v-col>
+      <v-col cols="12" sm="12">
+        <div class="d-flex justify-center mb-6">
+          <v-btn color="blue" class="mt-4">Load More</v-btn>
+        </div>
+      </v-col>
+      <v-col cols="12" id="page">
+        <div class="pre">
+          <v-row>
+            <v-col cols="12" sm="4">
+              <v-card class="mx-auto" max-width="344" height="">
+                <v-img src="i10.jpg" height="200px" cover></v-img>
+
+                <v-card-title> We provide you the best </v-card-title>
+
+                <v-card-subtitle>
+                  By Atlas-Expo | 24 April 2023
+                </v-card-subtitle>
+                <v-card-text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+                  rem saepe sapiente deleniti, odio non laborum fuga.
+                </v-card-text>
+              </v-card>
+            </v-col>
+            <v-col cols="12" sm="4">
+              <v-card class="mx-auto" max-width="344" height="">
+                <v-img src="i11.jpg" height="200px" cover></v-img>
+
+                <v-card-title> We provide you the best </v-card-title>
+
+                <v-card-subtitle>
+                  By Atlas-Expo | 24 April 2023
+                </v-card-subtitle>
+                <v-card-text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+                  rem saepe sapiente deleniti, odio non laborum fuga.
+                </v-card-text>
+              </v-card>
+            </v-col>
+            <v-col cols="12" sm="4">
+              <v-card class="mx-auto" max-width="344" height="">
+                <v-img src="i12.jpg" height="200px" cover></v-img>
+
+                <v-card-title> We provide you the best </v-card-title>
+
+                <v-card-subtitle>
+                  By Atlas-Expo | 24 April 2023
+                </v-card-subtitle>
+                <v-card-text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+                  rem saepe sapiente deleniti, odio non laborum fuga.
+                </v-card-text>
+              </v-card>
             </v-col>
           </v-row>
         </div>
       </v-col>
+      <v-col cols="12" id="biog">
+        <div class="hire">
+          <v-row>
+            <v-col cols="12" sm="8">
+              <h1 class="mt-9">Hire me for your awesome project</h1>
+              <p class="text-grey">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+                itaque, eaque molestiae deleniti, earum voluptate eos id dicta
+                at, blanditiis
+              </p>
+            </v-col>
+            <v-col cols="12" sm="4">
+              <v-btn color="#FBDF7E" class="mt-15">Hire Me</v-btn>
+            </v-col>
+          </v-row>
+        </div>
+      </v-col>
+      <v-col cols="12" sm="12" class="px-16" id="contact">
+        <v-row>
+          <v-col cols="12" sm="4">
+            <div class="child">
+              <h1>Contact info.</h1>
+              <v-btn
+                icon="fas fa-map-marker-alt"
+                color=""
+                class="mt-10"
+                variant="outlined"
+              ></v-btn
+              ><br />
+              <span class="text-caption">C. Valencia,yourArea Dream </span
+              ><br />
+              <v-btn
+                icon="fas fa-phone-alt"
+                color=""
+                class="mt-10"
+                variant="outlined"
+              ></v-btn
+              ><br />
+              <span class="text-caption">0034 - 632 34 36 58 </span> <br />
+              <span class="text-caption">0034 - 623 54 23 21 </span> <br />
+              <v-btn
+                icon="fas fa-envelope"
+                color=""
+                class="mt-10"
+                variant="outlined"
+              ></v-btn
+              ><br />
+              <span class="text-caption">atlas.expo05@gmail.com </span> <br />
+              <span class="text-caption">oukassou.jgr@gmail.com </span> <br />
+            </div>
+          </v-col>
+          <v-col cols="12" sm="8">
+            <h1 class="mt-8">Send your message</h1>
+            <v-divider></v-divider>
+            <span class="text-caption"
+              >Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+              fugiat officia, odio eaque exercitationem libero nesciunt placeat,
+              repellat obcaecati sed tenetur! Est labore aliquam amet
+              consequatur necessitatibus fugit obcaecati facilis!</span
+            >
+            <v-row class="mt-10">
+              <v-col cols="12" sm="6">
+                <v-text-field
+                  label="Name"
+                  persistent-hint
+                  variant="outlined"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-text-field
+                  label="Phone No"
+                  persistent-hint
+                  variant="outlined"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-textarea
+              label="Message"
+              persistent-hint
+              variant="outlined"
+            ></v-textarea>
+            <v-btn color="primary" class="mt-2">Submit Now</v-btn>
+          </v-col>
+        </v-row>
+      </v-col>
     </v-container>
+    <FooterView />
   </v-app>
 </template>
 
@@ -113,6 +323,7 @@ import { defineComponent } from "vue";
 
 // Components
 import NavBar from "../components/NavBar.vue";
+import FooterView from "../components/FooterView.vue";
 
 export default defineComponent({
   name: "HomeView",
@@ -145,6 +356,7 @@ export default defineComponent({
   },
   components: {
     NavBar,
+    FooterView,
   },
 });
 </script>
@@ -188,11 +400,11 @@ export default defineComponent({
   width: 356px;
   height: 300px;
   background-color: rgb(38, 78, 239);
-  border-radius: 50% 50% 50% 50% / 60% 60% 40% 0%;
+  border-radius: 50% 50% 50% 50% / 60% 60% 0% 10%;
 }
 .first {
   width: 100%;
-  height: 200px;
+  height: 280px;
   text-align: center;
   padding: 2rem 2rem;
 }
@@ -213,11 +425,136 @@ export default defineComponent({
   padding: 0 200px;
   background-color: #f5f5f5;
 }
-.hiro {
+.hire {
   width: 100%;
   height: 200px;
   padding: 0 200px;
   background-color: #e9e9e9;
   margin-top: -24px;
+}
+.hoverMe {
+  position: relative;
+  padding: 1em 1.8em;
+  outline: none;
+  border: 1px solid #303030;
+  background: #061bd8;
+  color: #ead70ad0;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  font-size: 15px;
+  overflow: hidden;
+  transition: 0.2s;
+  border-radius: 20px;
+  cursor: pointer;
+  font-weight: bold;
+}
+.hoverMe:hover {
+  box-shadow: 0 0 10px #caf00a, 0 0 25px #ff4400, 0 0 50px #ae00ff;
+  transition-delay: 0.6s;
+}
+.hoverMe span {
+  position: absolute;
+}
+
+.hoverMe span:nth-child(1) {
+  top: 0;
+  left: -100%;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(90deg, transparent, #ae00ff);
+}
+
+.hoverMe:hover span:nth-child(1) {
+  left: 100%;
+  transition: 0.7s;
+}
+
+.hoverMe span:nth-child(3) {
+  bottom: 0;
+  right: -100%;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(90deg, transparent, #001eff);
+}
+
+.hoverMe:hover span:nth-child(3) {
+  right: 100%;
+  transition: 0.7s;
+  transition-delay: 0.35s;
+}
+
+.hoverMe span:nth-child(2) {
+  top: -100%;
+  right: 0;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(180deg, transparent, #ae00ff);
+}
+
+.hoverMe:hover span:nth-child(2) {
+  top: 100%;
+  transition: 0.7s;
+  transition-delay: 0.17s;
+}
+
+.hoverMe span:nth-child(4) {
+  bottom: -100%;
+  left: 0;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(360deg, transparent, #001eff);
+}
+
+.hoverMe:hover span:nth-child(4) {
+  bottom: 100%;
+  transition: 0.7s;
+  transition-delay: 0.52s;
+}
+
+.hoverMe:active {
+  background: #ae00af;
+  background: linear-gradient(to top right, #ae00af, #001eff);
+  color: #bfbfbf;
+  box-shadow: 0 0 8px #ae00ff, 0 0 8px #001eff, 0 0 8px #ae00ff;
+  transition: 0.1s;
+}
+
+.hoverMe:active
+  span:nth-child(1)
+  span:nth-child(2)
+  span:nth-child(2)
+  span:nth-child(2) {
+  transition: none;
+  transition-delay: none;
+}
+.mouse-btn {
+  margin: 10px auto;
+  width: 40px;
+  height: 80px;
+  /*border: 3px solid rgba(14, 14, 184, 0.918);*/
+  border-radius: 20px;
+  display: flex;
+}
+
+.mouse-scroll {
+  display: block;
+  width: 20px;
+  height: 20px;
+  /*background: linear-gradient(170deg, rgba(248, 231, 2, 0.918), rgb(234, 5, 5));
+  border-radius: 50%;*/
+  margin: auto;
+  animation: scrolling13 1s linear infinite;
+}
+
+@keyframes scrolling13 {
+  0% {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateY(20px);
+  }
 }
 </style>
